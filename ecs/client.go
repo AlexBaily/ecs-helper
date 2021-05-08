1c1361b82538259f9b9d114e53aa95fa28a40d95
AlexBaily <alex.d.baily@gmail.com>
2021-05-08 22:06:19 +0000

    Add ecs client initialistion

--- ecs/client.go
+++ ecs/client.go
@@ -1,1 +1,29 @@
 package ecs
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/aws/aws-sdk-go/service/ecs/ecsiface"
+)
+
+//ecsInt is the ecs interface struct that holds the ECSAPI, used for mocking.
+type ecsInt struct {
+	Client ecsiface.ECSAPI
+}
+
+//ecsClient holds the reference to the ECS interface we are using in the package.
+var ecsClient *ecsInt
+
+//Configure the session with the ECS service client.
+func configureECS() {
+	ecsClient = new(ecsInt)
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+    svc := ecs.New(sess)
+	ecsClient.Client = ecsiface.ECSAPI(svc)
+}
+
+func init() {
+	configureECS()
+}
