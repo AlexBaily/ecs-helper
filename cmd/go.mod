818a5f481d098b7fd7f7a09a75ea98810f73589f
AlexBaily <alex.d.baily@gmail.com>
2021-05-08 21:43:19 +0000

    Add cobra command line commands.

---
+++ cmd/go.mod
@@ -0,0 +1,5 @@
+module github.com/AlexBaily/ecs-helper/cmd
+
+go 1.15
+
+require github.com/spf13/cobra v1.1.3
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/AlexBaily/ecs-helper/cmd"
-
-	"github.com/spf13/cobra"
 )
 
 func main() {
